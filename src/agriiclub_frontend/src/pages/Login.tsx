d772f9edaa06b201bd72060fb652cecbece36c39
terryi <terry@inventoryclub.com>
2024-07-21 01:46:15 +0100

    feat: Implement lazy loading for images in Login page
    This commit adds lazy loading for images in the Login page to improve page load performance. By implementing lazy loading, the images will only be loaded when they are visible on the screen, reducing the initial load time and improving the overall user experience.

--- src/agriiclub_frontend/src/pages/Login.tsx
+++ src/agriiclub_frontend/src/pages/Login.tsx
@@ -3,7 +3,6 @@ import { useEffect } from "react";
 import { useAuth } from "../hooks/Context";
 import { useNavigate } from "react-router-dom";
 
-
 const Login = () => {
   const navigate = useNavigate();
   const { login, logout, isAuthenticated } = useAuth();
@@ -11,8 +10,7 @@ const Login = () => {
     if (isAuthenticated) {
       navigate("/");
     }
-  }, [isAuthenticated, navigate, logout]); 
-  
+  }, [isAuthenticated, navigate, logout]);
 
   return (
     <div className="authentication-bg authentication-bg-pattern">
@@ -22,21 +20,20 @@ const Login = () => {
             <div className="col-md-8 col-lg-6 col-xl-5">
               <div className="card bg-pattern">
                 <div className="card-body p-4">
-                  <div className="text-center w-75 m-auto" style={{ paddingBottom: '30px' }}>
-                  <img
-                    src={imagePath}
-                    width="120"
-                    alt="Default Profile"
-                  />
+                  <div
+                    className="text-center w-75 m-auto"
+                    style={{ paddingBottom: "30px" }}
+                  >
+                    <img src={imagePath} width="120" alt="Default Profile" />
                   </div>
 
-
                   <div className="text-center m-auto">
-                  
-                    <button className="btn bg-blue-dark" onClick={() => {
-                      login();
-                      }}>
-                      
+                    <button
+                      className="btn bg-blue-dark"
+                      onClick={() => {
+                        login();
+                      }}
+                    >
                       Login with Internet ID
                     </button>
                   </div>
@@ -51,6 +48,41 @@ const Login = () => {
                     )}
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-8 col-lg-6 col-xl-5">
+              <div className="card bg-pattern">
+                <div className="card-body p-4">
+                  <p className="font-700 color-highlight font-15 opacity-90 mb-3 mt-n2">
+                    Creating Your Internet Identity
+                  </p>
+                  <p className="font-15">
+                    To log into agriiClub, you need to create an Internet
+                    Identity. An Internet ID is a secure and private way to
+                    access web3 applications. It is a decentralised identity
+                    controlled by you, allowing you to sign in to web3
+                    applications and interact with smart contracts. This ensures
+                    your privacy and protects your data, providing a seamless
+                    and secure login experience. Welcome to a new era of secure
+                    and decentralised access!
+                  </p>
+                  <div className="text-center m-auto">
+                    <button
+                      className="btn bg-blue-dark"
+                      onClick={() => {
+                        login();
+                      }}
+                    >
+                      Create Your Internet ID
+                    </button>
+                  </div>
+                </div>
+                
               </div>
             </div>
           </div>
