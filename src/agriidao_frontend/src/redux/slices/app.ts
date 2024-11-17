747fcd9b7b0d6d49ce84a3fe3d4d432448fc41fd
terryi <terry@inventoryclub.com>
2024-11-17 08:52:22 +0200

    Enhance app slice: add marketLocationAgent state and corresponding reducer

--- src/agriidao_frontend/src/redux/slices/app.ts
+++ src/agriidao_frontend/src/redux/slices/app.ts
@@ -1,20 +1,35 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit"; 
-import { CampaignUserRequest, UserSocialMediaRequest, BountyPoint, UserSocialMedia, Campaign } from "../../../../declarations/bounty/bounty.did";
-import { Profile, ProfileRequest, User } from "../../../../declarations/user/user.did";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import {
+  CampaignUserRequest,
+  UserSocialMediaRequest,
+  BountyPoint,
+  UserSocialMedia,
+  Campaign,
+} from "../../../../declarations/bounty/bounty.did";
+import {
+  Profile,
+  ProfileRequest,
+  User,
+} from "../../../../declarations/user/user.did";
+import { 
+  MarketLocationAgent, 
+  MarketLocation, 
+  MarketLocationCommodity,
+ } from "../../../../declarations/commodity/commodity.did";
 
+export interface GlobalState {
+  selectedCampaign: Campaign | null;
+  campaignUserRequest: CampaignUserRequest | null;
+  userSocialMediaRequest: UserSocialMediaRequest | null;
+  userSocialMedia: UserSocialMedia | null;
+  user: User | null;
+  profileRequest: ProfileRequest | null;
+  profile: Profile | null;
+  bountyPoints: BountyPoint[] | null;
+  marketLocationAgent: MarketLocationAgent | null;
+}
 
-export interface GlobalState { 
-selectedCampaign : Campaign | null,
- campaignUserRequest : CampaignUserRequest | null,
- userSocialMediaRequest : UserSocialMediaRequest | null,
- userSocialMedia : UserSocialMedia | null,
- user : User | null,
- profileRequest : ProfileRequest | null,
- profile : Profile | null,
- bountyPoints : BountyPoint[] | null,
-}; 
- 
-const initialState: GlobalState = { 
+const initialState: GlobalState = {
   selectedCampaign: null,
   campaignUserRequest: null,
   userSocialMediaRequest: null,
@@ -23,51 +38,65 @@ const initialState: GlobalState = {
   profileRequest: null,
   profile: null,
   bountyPoints: null,
-}; 
- 
-export const appSlice = createSlice({ 
-  name: "app", 
-  initialState, 
-  reducers: { 
+  marketLocationAgent: null,
+};
+
+export const appSlice = createSlice({
+  name: "app",
+  initialState,
+  reducers: {
     setSelectedCampaign(state, action: PayloadAction<Campaign | null>) {
       state.selectedCampaign = action.payload;
-      },
-    setCampaignUserRequest: (state, action: PayloadAction<CampaignUserRequest | null>) => {
+    },
+    setCampaignUserRequest: (
+      state,
+      action: PayloadAction<CampaignUserRequest | null>
+    ) => {
       state.campaignUserRequest = action.payload;
-      },
-    setUserSocialMediaRequest: (state, action: PayloadAction<UserSocialMediaRequest | null>) => {
+    },
+    setUserSocialMediaRequest: (
+      state,
+      action: PayloadAction<UserSocialMediaRequest | null>
+    ) => {
       state.userSocialMediaRequest = action.payload;
-      },
-    setUserSocialMedia: (state, action: PayloadAction<UserSocialMedia | null>) => {
+    },
+    setUserSocialMedia: (
+      state,
+      action: PayloadAction<UserSocialMedia | null>
+    ) => {
       state.userSocialMedia = action.payload;
-      },
+    },
     setUser: (state, action: PayloadAction<User | null>) => {
       state.user = action.payload;
-      },
-    setProfileRequest: (state, action: PayloadAction<ProfileRequest | null>) => {
+    },
+    setProfileRequest: (
+      state,
+      action: PayloadAction<ProfileRequest | null>
+    ) => {
       state.profileRequest = action.payload;
-      },
+    },
     setProfile: (state, action: PayloadAction<Profile | null>) => {
       state.profile = action.payload;
-      },
+    },
     setBountyPoints: (state, action: PayloadAction<BountyPoint[] | null>) => {
       state.bountyPoints = action.payload;
-      }
-  }, 
-}); 
- 
-export const { 
+    },
+    setMarketLocationAgent: (state,action: PayloadAction<MarketLocationAgent | null>) => {
+      state.marketLocationAgent = action.payload;
+    },
+  },
+});
+
+export const {
   setSelectedCampaign,
   setCampaignUserRequest,
   setUserSocialMediaRequest,
   setUserSocialMedia,
   setUser,
   setProfileRequest,
-  setProfile, 
+  setProfile,
   setBountyPoints,
-} = appSlice.actions; 
+  setMarketLocationAgent,
+} = appSlice.actions;
 
-
-
- 
 export default appSlice.reducer;
