e6b95ca2ad3c808835e5d2d1785aac543f27c88d
terryi <terry@inventoryclub.com>
2025-02-23 21:21:33 +0200

    refactor: enhance DesktopTopBar component layout and improve search functionality

--- src/agriidao_frontend/src/components/desktop/TopBar.tsx
+++ src/agriidao_frontend/src/components/desktop/TopBar.tsx
@@ -2,39 +2,53 @@ import React from "react";
 import DProfileClick from "../../pages/desktop/profile/DProfileClick";
 
 interface DesktopTopBarProps {
+  onToggle: () => void;
+}
 
-    onToggle: () => void;
-  
-  }
-
-  const DesktopTopBar: React.FC<DesktopTopBarProps> = ({ onToggle }) => {
-    return (
-        <div className="top-header">
-          <div className="header-bar d-flex justify-content-between">
-            <div className="d-flex align-items-center">
-              <a href="#" className="logo-icon me-3">
-                <img src="assets/images/logo-icon.png" height="30" alt="Logo" />
-              </a>
-              <button onClick={onToggle} className="btn btn-soft-light"><i className="mdi mdi-menu"></i></button>
-
+const DesktopTopBar: React.FC<DesktopTopBarProps> = ({ onToggle }) => {
+  return (
+    <div className="top-header">
+      <div className="header-bar d-flex justify-content-between">
+        <div className="d-flex align-items-center">
+          <button onClick={onToggle} className="btn btn-soft-light">
+            <i className="mdi mdi-menu"></i>
+          </button>
+          <div className="search-bar p-0 d-none d-md-block ms-2">
+            <div id="search" className="menu-search mb-0">
+              <form
+                role="search"
+                method="get"
+                id="searchform"
+                className="searchform"
+              >
+                <div>
+                  <input
+                    type="text"
+                    className="form-control border rounded"
+                    name="s"
+                    id="s"
+                    placeholder="Ask Yoma..."
+                  />
+                  <input type="submit" id="searchsubmit" value="Search" />
+                </div>
+              </form>
             </div>
-    
-            <ul className="list-unstyled mb-0 d-flex">
-              <li className="list-inline-item">
-                <a href="#" className="btn btn-icon btn-soft-light">
-                  <i className="ti ti-bell"></i>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#" className="btn btn-soft-light">
-                  {/* <img src="assets/images/client/05.jpg" className="" alt="User" /> */}
-                  <DProfileClick/>
-                </a>
-              </li>
-            </ul>
           </div>
         </div>
-      );
+
+        <ul className="list-unstyled mb-0 d-flex">
+          <li className="list-inline-item">
+            <a href="#" className="btn btn-soft-light">
+              <i className="mdi mdi-robot"></i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <DProfileClick />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default DesktopTopBar;
