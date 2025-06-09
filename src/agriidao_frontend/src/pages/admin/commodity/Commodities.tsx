49f0d79413bb21d1ed3a701b6012c735bd823d1b
terryi <terry@inventoryclub.com>
2025-06-09 07:53:57 +0100

    feat: Add commodity management pages including Commodities, CommodityPrices, CommoditySummary, MarketAgents, MarketSummary, and Markets
    - Implemented Commodities page to display a list of commodities with options to view details.
    - Created CommodityPrices page to show market prices for selected commodities.
    - Developed CommoditySummary page for detailed view of a specific commodity and its market locations.
    - Added MarketAgents page to manage agents associated with market locations.
    - Introduced MarketSummary page to summarize market details and associated commodities.
    - Built Markets page to list all market locations with options to view details.
    - Implemented forms for adding market agents and market locations with validation.
    - Created reusable components for displaying commodity and user subscriptions.

---
+++ src/agriidao_frontend/src/pages/admin/commodity/Commodities.tsx
@@ -0,0 +1,118 @@
+import { useEffect, useState } from "react";
+import { Link, NavLink, useParams } from "react-router-dom";
+import { useAuth } from "../../../hooks/Context";
+import { Commodity } from "../../../../../declarations/commodity/commodity.did";
+import CategoryName from "../../../components/CategoryName";
+
+const Commodities = () => {
+  const { commodityActor } = useAuth(); 
+  const { id } = useParams(); 
+  const [commodities, setCommodities] = useState<Commodity[]>([]); 
+  const [openForm, setOpenForm] = useState(false);
+  const [commoditySaved, setCommoditySaved] = useState(false);
+
+  // get latest category and commodities
+  useEffect(() => {
+    getAllLatestCommodities();
+  }, [id]);
+  
+  // get commodities  
+  const getAllLatestCommodities = async () => {
+    let res = await commodityActor?.getAllLatestCommodities(); 
+    setCommodities(res || []);
+  };
+
+  return (
+    <>
+      {/* {openForm && (
+        <AddCommodity {...{ setOpenForm, setCommoditySaved }} />
+      )} */}
+      
+      <div className="d-flex align-items-center justify-content-between">
+        <div>
+          <h5 className="mb-0">Commodities</h5>
+        </div>
+
+        <div className="mb-0 position-relative">
+          <NavLink
+            to={`/d/start-coop/`}
+            className="btn btn-outline-dark col-sm-12"
+          >
+            Add Commodity
+          </NavLink>
+        </div>
+      </div>
+
+      <div className="col-xl-12 mt-4">
+        <div className="card border-0">
+          <div
+            className="table-responsive shadow rounded-bottom"
+            data-simplebar
+            style={{ height: "545px" }}
+          >
+            <table className="table table-center bg-white mb-0">
+              <thead>
+                <tr>
+                  <th className="border-bottom p-3" style={{ width: "150px" }}>
+                    Name
+                  </th>
+                  <th className="border-bottom p-3" style={{ width: "150px" }}>
+                    Ticker
+                  </th>
+                  <th className="border-bottom p-3" style={{ width: "150px" }}>
+                    Commodity Type
+                  </th>
+                  <th className="border-bottom p-3" style={{ width: "150px" }}>
+                    Image
+                  </th>
+                  <th className="border-bottom p-3" style={{ width: "150px" }}>
+                    View
+                  </th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {commodities && commodities.length > 0 ? (
+                  commodities.map((commodity, index) => (
+                    <tr className="even" key={index}>
+                      <td className="p-3">
+                      <span className="ms-2">{commodity.name}</span></td>
+                      <td className="p-3">{commodity.ticker}</td>
+                      <td className="p-3"><CategoryName id={commodity.acCategoryId}/></td>
+                      <td className="p-3">
+                        {commodities && (
+                          <img // TODO: dislay commodity image 
+                            src={
+                              commodity.commodityPic === ""
+                              ? "/default-image.png"
+                              : commodity.commodityPic
+                            }
+                            alt="commodity pic"
+                            width="80"
+                            className="img-fluid avatar-xl rounded"
+                            />
+                        )}
+
+                      </td>
+
+                      <td className="p-3">
+                        <Link to={`/commodity-summary/${commodity.id}`}>View</Link>
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan={5}  className="text-center">No data available</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Commodities; //whatever the name of the functional component declared above should be used here
+
---
+++ src/agriidao_frontend/src/pages/admin/commodity/CommodityPrices.tsx
@@ -0,0 +1,177 @@
+import { useEffect, useState } from "react";
+import { Link, NavLink, useParams } from "react-router-dom";
+import { useAuth } from "../../../hooks/Context";
+import { Commodity, MarketLocation, MarketLocationCommodity, MarketPrice } from "../../../../../declarations/commodity/commodity.did";
+
+function CommodityPrices() {
+  const { commodityActor } = useAuth();
+  const { id } = useParams();
+  const [mLCommodity, setMLCommodity] = useState<MarketLocationCommodity | null>(null);
+  const [commoditySubs, setCommoditySubs] = useState<MarketLocationCommodity | null>(null);
+  const [prices, setPrices] = useState<MarketPrice[] | null>(null);
+  const [market, setMarket] = useState<MarketLocation | null>(null);
+    const [commodity, setCommodity] = useState<Commodity | null>(null);
+
+
+  useEffect(() => {
+    getMLCommodity();
+  }, [id]);
+
+  const getMLCommodity = async () => {
+    if (!id || !commodityActor) {
+      console.error("ID or commodityActor is null");
+      return;
+    }
+
+    const res = await commodityActor.getMarketLocationCommodityById(id);
+    if ("ok" in res) {
+      setMLCommodity(res.ok);
+      setCommoditySubs(res.ok);
+    } else {
+      console.error(res.err);
+    }
+  };
+
+  useEffect(() => {
+    getCommodity();
+    }, [mLCommodity]);
+
+    const getCommodity = async () => {
+        if (!mLCommodity || !commodityActor) {
+            console.error("mlCommodity or commodityActor not found");
+            return;
+        }
+        const res = await commodityActor.getCommodityLatest(mLCommodity.commodityId);
+        console.log("commodity", res);
+        if ("ok" in res) {
+            setCommodity(res.ok);
+        }
+        else {
+            console.error(res.err);
+        }
+    };
+
+  useEffect(() => {
+    getMarketLocation();
+    }, [mLCommodity]);
+
+    const getMarketLocation = async () => {
+    if (!mLCommodity || !commodityActor) {
+        console.error("mlCommodity or commodityActor not found");
+        return;
+    }
+    const res = await commodityActor.getMarketLocationLatest(mLCommodity.marketLocationId);
+    console.log("market location", res);
+    if ("ok" in res) {
+        setMarket(res.ok);
+    }
+    else {
+        console.error(res.err);
+    }
+};
+
+
+  useEffect(() => {
+    getPrices();
+  }, [mLCommodity]);
+
+  const getPrices = async () => {
+    if (!mLCommodity || !commodityActor) {
+      console.error("mlCommodity or commodityActor not found");
+      return;
+    }
+    const res = await commodityActor.getMarketPriceByMarketCommodityId(
+      mLCommodity.id
+    );
+    setPrices(res);
+    
+  };
+
+  return (
+    <>
+      <div className="d-flex align-items-center justify-content-between">
+              <div>
+                <h5 className="mb-0">Commodity Price</h5>
+              </div>
+            </div>
+
+            <div className="row">
+        <div className="col-xl-12 mt-4">
+          <div className="card rounded shadow border-0 p-4">
+            <div className="mt-4">
+            <h4 className="text-uppercase">{commodity?.name}</h4>
+              <br />
+              <dl className="row">
+                  <dt className="col-sm-5">Market</dt>
+                  <dd className="col-sm-7">{market?.name}</dd>
+                </dl>
+            </div>
+
+            <div className="mt-3">
+              <NavLink
+                to={`/market-locations`}
+                className="btn btn-outline-dark col-sm-12"
+              >
+                Markets
+              </NavLink>
+            </div>
+            <div className="mt-3">
+              <NavLink
+                to={`/market-locations`}
+                className="btn btn-outline-dark col-sm-12"
+              >
+                Market Agents
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>          
+
+          <div className="col-xl-8">
+          <div className="col-xl-12 mt-4">
+          <div className="card rounded shadow border-0 p-4">
+            <div className="d-flex justify-content-between mb-4">
+              <h5 className="mb-3 bg-light p-2 row">
+                 Daily Market Prices
+              </h5>
+
+                <table className="table table-center bg-white mb-0">
+                  <thead>
+                    <tr>
+                      <th className="border-bottom p-3">
+                        Price Per KG
+                      </th>
+                      <th className="border-bottom p-3">
+                        Date
+                      </th>
+                      <th className="border-bottom p-3">View</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {prices && prices.length > 0 ? (
+                      prices?.map((price, index) => (
+                        <tr className="even" key={index}>
+                          <td>{price.pricePerKg}</td>
+                          {/* <td>{price.unit}</td> */}
+                          <td>{price.timeStamp}</td>
+                          <td>
+                            <Link to={`/market-summary/${price.id}`}>View</Link>
+                          </td>
+                        </tr>
+                      ))
+                    ) : (
+                      <tr>
+                        <td colSpan={5}>No Commodities Found</td>
+                      </tr>
+                    )}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+    </>
+  );
+}
+
+export default CommodityPrices;
